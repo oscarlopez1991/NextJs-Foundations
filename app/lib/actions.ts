369cc49380ea050e95ae252a936f2223d4763b60
Oscar Lopez Martínez <oscar.bcn.1991@gmail.com>
2026-06-24 19:09:21 +0200

    Refactor invoice forms to improve error handling and accessibility. Removed console logs from createInvoice function. Added error messages for amount and status fields in both create and edit invoice forms, enhancing user feedback and compliance with accessibility standards.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -55,8 +55,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
     status: formData.get('status'),
   });
 
-  console.log(validatedFields);
- 
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
@@ -98,8 +96,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
       status: formData.get('status'),
     });
 
-    console.log(validatedFields);
-   
     // If form validation fails, return errors early. Otherwise, continue.
     if (!validatedFields.success) {
       return {
